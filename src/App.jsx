081deae70ded9a2efdcaf23dcbe4543317d4f807
bublibub <shourya.batra@gmail.com>
2024-10-12 21:02:09 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Main from "./components/Main/Main";
 // import Form from './components/Form/Form'
